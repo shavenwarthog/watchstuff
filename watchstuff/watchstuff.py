9bcdbe9b0e044cc415577db30669b8d6d05500e2
John Mitchell <johnlmitchell@gmail.com>
2012-11-14 10:25:26 -0800

    add "ignore_twoline" for skipping parts of tracebacks

--- watchstuff/watchstuff.py
+++ watchstuff/watchstuff.py
@@ -20,8 +20,10 @@ CONFIG = '''
 # ignore lines with this regular expression pattern:
 #ignore_pat: elasticd.+
 ignore_pat:
-  lib/python2
   raise.JSONDecodeError
+
+ignore_twoline:
+  lib/python2
 
 # colorize words:
 colori: error,white,on_red
@@ -42,6 +44,9 @@ colori: warning,red
 #  INFO,bold
 #  ERROR,red
 #  entropy,black,on_yellow
+
+# shortname:
+#  enterpriseapp.log=ENT
 '''
 
 
@@ -60,36 +65,40 @@ def should_ignore(config, line):
             + r')\b')
         m = ignore.search(line)
         if m is not None:
-            return True
+            return True, False
     ignore_str = config.get('ignore_pat', None)
     if ignore_str:
         pats = '|'.join(ignore_str.split())
-        return bool( re.search(r'(' + pats + r')\b', line) )
-    return False
+        if re.search(r'(' + pats + r')', line):
+            return True, False
+    ignore_str = config.get('ignore_twoline', None)
+    if ignore_str:
+        pats = '|'.join(ignore_str.split())
+        if re.search(r'(' + pats + r')', line):
+            return True, True
+    return False, False
 
 
-def test_should_ignore():
+def test_ignore():
     conf = dict(ignore='bogus')
-    eq_( should_ignore(conf, 'tasty'), False )
-    eq_( should_ignore(conf, 'this bogus line'), True )
+    eq_( should_ignore(conf, 'tasty'), (False,False) )
+    eq_( should_ignore(conf, 'this bogus line'), (True,False) )
 
+
+def test_ignore_pat():
     conf = dict(ignore_pat='g.+s')
-    eq_( should_ignore(conf, 'tasty'), False )
-    eq_( should_ignore(conf, 'this bogus line'), True )
-    eq_( should_ignore(conf, 'squishbogus'), True )
-    eq_( should_ignore(conf, 'guff'), False )
+    eq_( should_ignore(conf, 'tasty'), (False,False) )
+    eq_( should_ignore(conf, 'this bogus line'), (True,False) )
+    eq_( should_ignore(conf, 'squishbogus'), (True,False) )
+    eq_( should_ignore(conf, 'guff'), (False,False) )
+
+
+def test_ignore_twoline():
+    conf = dict(ignore_twoline='python')
+    eq_( should_ignore(conf, 'beer'), (False,False) )
+    eq_( should_ignore(conf, '/python2'), (True,True) )
 
                       
-    
-
-# def should_ignore(config, msg):
-#     ignore_str = config.get('ignore')
-#     if not ignore_str:
-#         return False
-#     ignore_pat = re.compile(r'('+ignore_str+r')')
-#     return ignore_pat.search(msg) != False
-
-
 def do_color(config, msg):
     color, on_color = None, None
     args = config.get('colori').split(',')
@@ -118,11 +127,15 @@ def do_colorpat(config, msg):
 
         
 def annotate(config, msg):
-    if should_ignore(config, msg):
-        return
+    """
+    Return fixed message, and whether to ignore the next line or not
+    """
+    thisline,twoline = should_ignore(config, msg)
+    if thisline:
+        return None,twoline
     msg = do_color(config, msg)
     msg = do_colorpat(config, msg)
-    return msg
+    return msg,False
 
 
 
@@ -156,7 +169,7 @@ def watchstuff(opts, args):
         '(?P<rest>	.+)',
         re.VERBOSE)
     name_pat = re.compile('==>.(.+).<==')
-
+    ignore_next = False
     try:
         last = None
         name = None
@@ -165,6 +178,9 @@ def watchstuff(opts, args):
             line = proc.readline()
             if not line:
                 break
+            if ignore_next:
+                ignore_next = False
+                continue
             m = name_pat.match(line)
             if m:
                 name = os.path.basename(m.group(1))
@@ -173,8 +189,6 @@ def watchstuff(opts, args):
             rest = line.rstrip()
             if info:
                 rest = info.group('rest')
-                # if should_ignore_proc(config, info.group(3)):
-                #     continue
             else:
                 if opts.verbose:
                     print '?',name,line.rstrip()
@@ -184,7 +198,7 @@ def watchstuff(opts, args):
                 print '-'*20, int(time.time()-last), 'sec'
                 print
             last = time.time()
-            msg = annotate(config, rest)
+            msg,ignore_next = annotate(config, rest)
             if msg:
                 if name:
                     print '{0}: {1}'.format(name,msg)
