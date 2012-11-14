dd84412d98243b0c85d51887945b0aab9e8902c0
John Mitchell <johnlmitchell@gmail.com>
2012-11-14 09:52:18 -0800

    fixed "ignore pattern" feature

--- watchstuff/watchstuff.py
+++ watchstuff/watchstuff.py
@@ -13,30 +13,32 @@ CONFIG = '''
 [default]
 
 # ignore lines with any of these words:
-ignore_proc: postfix CRON ntpd apid_stat.py pdns_recursor
+# ignore: postfix CRON ntpd apid_stat.py pdns_recursor
 
 # ignore lines with this regular expression pattern:
-ignore_proc_pat: elasticd.+
+#ignore_pat: elasticd.+
+# ignore_pat: lib/python
+ignore_pat: py
 
 # colorize words:
 colori: error,white,on_red
 colori: warning,red
 
 # colorize patterns:
-color_pat:
- client,bold
- Message.+as dev\S+,underline
+# color_pat:
+#  client,bold
+#  Message.+as dev\S+,underline
 
 # can repeat:
-color_pat:
- smtp_reply.+?]]],bold
+# color_pat:
+#  smtp_reply.+?]]],bold
 
 # Munin timestamp
-color_pat:
- ..../../..-..:..:..,underline
- INFO,bold
- ERROR,red
- entropy,black,on_yellow
+# color_pat:
+#  ..../../..-..:..:..,underline
+#  INFO,bold
+#  ERROR,red
+#  entropy,black,on_yellow
 '''
 
 
@@ -46,27 +48,43 @@ def parseconfig(configstr):
     return dict(par.items('default'))
 
 
-def should_ignore_proc(config, procname):
-    m = re.search(
-        r'('
-        + r'|'.join(config.get('ignore_proc','').split())
-        + r')\b',
-        procname)
-    if m:
-        return True
-    pats = '|'.join(config.get('ignore_proc_pat','').split())
-    m = re.search(r'(' + pats + r')\b', procname)
-    if m:
-        return True
+def should_ignore(config, line):
+    ignore_str = config.get('ignore', None)
+    if ignore_str:
+        ignore = re.compile(
+            r'\b('
+            + r'|'.join(ignore_str.split())
+            + r')\b')
+        m = ignore.search(line)
+        if m is not None:
+            return True
+    ignore_str = config.get('ignore_pat', None)
+    if ignore_str:
+        pats = '|'.join(ignore_str.split())
+        return bool( re.search(r'(' + pats + r')\b', line) )
     return False
 
+from nose.tools import eq_
+def test_should_ignore():
+    conf = dict(ignore='bogus')
+    eq_( should_ignore(conf, 'tasty'), False )
+    eq_( should_ignore(conf, 'this bogus line'), True )
 
-def should_ignore(config, msg):
-    ignore_str = config.get('ignore')
-    if not ignore_str:
-        return False
-    ignore_pat = re.compile(r'('+ignore_str+r')')
-    return ignore_pat.search(msg) != False
+    conf = dict(ignore_pat='g.+s')
+    eq_( should_ignore(conf, 'tasty'), False )
+    eq_( should_ignore(conf, 'this bogus line'), True )
+    eq_( should_ignore(conf, 'squishbogus'), True )
+    eq_( should_ignore(conf, 'guff'), False )
+
+                      
+    
+
+# def should_ignore(config, msg):
+#     ignore_str = config.get('ignore')
+#     if not ignore_str:
+#         return False
+#     ignore_pat = re.compile(r'('+ignore_str+r')')
+#     return ignore_pat.search(msg) != False
 
 
 def do_color(config, msg):
@@ -83,7 +101,7 @@ def do_color(config, msg):
     
 
 def do_colorpat(config, msg):
-    for colorpat in filter(None, config.get('color_pat').split('\n')):
+    for colorpat in filter(None, config.get('color_pat','').split('\n')):
         args = colorpat.split(',')
         word = args.pop(0)
         attrs = []
@@ -120,14 +138,22 @@ def watchstuff(opts, args):
     # WARNING: os.popen() is deprecated.  However, it works.
     proc = os.popen(' '.join(cmd + args))
 
-    # 
+    # TODO: move to external config file
     config = parseconfig(CONFIG)
     config.update( dict(
             ignore=opts.ignore,
             ) )
     
     # Sep 13 13:13:54 hostname procname 123:
-    linepat = re.compile('(... .. \S+) (\S+) (.+?:) (.+)')
+    # date_pat = '(... .. \S+) (\S+) (.+?:) (.+)'
+    # [12/Nov/2012 09:55:35] WARNING [django.request:142 get_response] ...
+    date_pat = re.compile(
+        '\['
+        '(?P<timestamp>	\S+ [0-9:]{8})\] \s+'
+        '(?P<rest>	.+)',
+        re.VERBOSE)
+    
+
     try:
         last = None
         pause_secs = 5
@@ -135,19 +161,21 @@ def watchstuff(opts, args):
             line = proc.readline()
             if not line:
                 break
-            info = linepat.match(line)
+            info = date_pat.match(line)
             rest = line.strip()
             if info:
-                rest = info.group(4)
-                if should_ignore_proc(config, info.group(3)):
+                rest = info.group('rest')
+                # if should_ignore_proc(config, info.group(3)):
+                #     continue
+            else:
+                if opts.verbose:
+                    print '?',line.rstrip()
                     continue
             if last and time.time()-last > pause_secs:
                 print
                 print '-'*20, int(time.time()-last), 'sec'
                 print
             last = time.time()
-            # print colored(timestamp,attrs=['underline']),
-            # print colored(procname,'yellow'),
             msg = annotate(config, rest)
             if msg:
                 print msg
@@ -162,6 +190,8 @@ def main():
                       const=True, help="follow file")
     parser.add_option('--ignore', dest="ignore", default='',
                       help="ignore lines with pattern")
+    parser.add_option('-v', '--verbose', dest="verbose", action='store_true',
+                      help="output nonmatched lines")
     (options, args) = parser.parse_args()
     return watchstuff(options, args)
 
